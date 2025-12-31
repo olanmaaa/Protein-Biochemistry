530f708c643c7c6262119a84e3de8239368728a6
olanmaaa <olanmaspam@gmail.com>
2025-12-31 13:58:24 +0000

    task code/importing data set

---
+++ taskcode.R
@@ -0,0 +1,4 @@
+#importing the datasets
+fold_data<- read.delim('https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/datasets/foldX.tsv', header= TRUE, sep = '')
+sift_data<- read.delim('https://raw.githubusercontent.com/HackBio-Internship/public_datasets/main/R/datasets/sift.tsv', header=TRUE,sep='')
+
